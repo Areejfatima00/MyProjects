fdfeb4beb69ac87de71f5ccf5be96d350ecc7eb0
Areejfatima00 <areej4761@gmail.com>
2023-09-09 00:54:48 +0500

    my project

---
+++ class3.js
@@ -0,0 +1,21 @@
+"use strict";
+/*var output:boolean=5<1 && 15>10;
+if( 5<1 && 15>10){
+  console.log(output);
+}
+*/
+//ak if k andr ak r if a jai to usy nested if bolty hn
+/*if(2<5){
+    console.log("parent");
+if(6<1){
+    console.log("Child");
+
+if(2==2){
+    console.log("nested child");
+}}}*/
+if (2 > 5) {
+    console.log("two<");
+}
+else {
+    console.log("two>");
+}
